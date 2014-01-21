7111a40adfd3f36255c3c2de74d3d22aba5702d5
Wouter Haakmeester <whaakmeester@gmail.com>
2014-01-21 14:02:31 +0100

    Tekenen van de circel met de actuele waardes van de simulatie

---
+++ Vossen en Konijnen/src/main/Runner.java
@@ -0,0 +1,13 @@
+package main;
+
+import model.*;
+import view.*;
+import controller.*;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		Simulator s1 = new Simulator();
+	}
+
+}
